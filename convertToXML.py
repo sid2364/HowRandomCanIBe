022316d9ba0a349be5ef630c5600efc340147c24
Siddharth Sahay <sahaysid@gmail.com>
2017-09-02 14:53:53 +0530

    Convert database schema into XML based on heirarchy

--- convertToXML.py
+++ convertToXML.py
@@ -1,21 +1,18 @@
+#!/usr/bin/python
+
+import xml.etree.cElementTree as ET
 import sqlite3
 import json
+import sys
 from lxml import etree
 
-conn = sqlite3.connect('generic_employee.db')
-conn.executescript('DROP TABLE IF EXISTS EMPLOYEES;')
-conn.execute('''CREATE TABLE EMPLOYEES
-         (ID INT PRIMARY KEY     NOT NULL,
-         NAME           TEXT    NOT NULL,
-         MANAGERID            INT);''')
-conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) \
-      VALUES (1, 'A', 2 )");
-conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) \
-      VALUES (2, 'B', 4 )");
-conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) \
-      VALUES (3, 'C', 4 )");
-conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) \
-      VALUES (4, 'D', NULL )");
+xml_file = "einfo.xml"
+
+eid = 'id'
+name = 'name'
+managerid = 'managerid'
+subords = 'subords'
+topmost='topmost'
 
 
 def finditem(obj, key):
@@ -39,41 +36,84 @@ def replaceitem(obj, key, new_item):
 				v[key] = new_item
 	return obj
 
+def parse_dict(data_dict):
+	parsed_data_dict = {}
+	for key in data_dict:
+		parsed_data_dict[key] = {}
+		parsed_data_dict[key][subords] = []
 
-eid = 'id'
-name = 'name'
-managerid = 'managerid'
-subords = 'subords'
-topmost='topmost'
+	for key in data_dict:
+		mid = data_dict[key][managerid]
+		for i in data_dict:
+			if i == key:
+				continue
+			if mid == data_dict[i][eid]:
+				parsed_data_dict[i][subords].append(data_dict[key])
+				#data_dict[key] = replaceitem(data_dict[key], topmost, mid)
+				data_dict[i][subords].append(data_dict[key])
+				del parsed_data_dict[key]
 
-data_dict = {}
-cursor = conn.execute("SELECT ID, NAME, MANAGERID from EMPLOYEES")
-for row in cursor:
-	data_dict[row[0]] = {eid:row[0], name:row[1], managerid:row[2], subords:[], topmost:-1}
+	return parsed_data_dict
 
-print(json.dumps(data_dict, indent=4))
 
-parsed_data_dict = dict(data_dict)
 
-for key in data_dict:
-	parsed_data_dict[key] = {}
-	parsed_data_dict[key][subords] = []
+def recurseThrough(pd, data_dict, xml_root):
+	#print("In recurseThrough: ", pd[name], pd[eid])
+	node = ET.SubElement(xml_root, "Node", name=pd[name], eid=str(pd[eid]))
+	for sub in pd[subords]:
+		recurseThrough(sub, data_dict, node)
+	return
 
-for key in data_dict:
-	mid = data_dict[key][managerid]
-	for i in data_dict:
-		if i == key:
-			continue
-		if mid == data_dict[i][eid]:
-			parsed_data_dict[i][subords].append(data_dict[key])
-			#data_dict[key] = replaceitem(data_dict[key], topmost, mid)
-			data_dict[i][subords].append(data_dict[key])
-			del parsed_data_dict[key]
+def createXml(parsed_data_dict, data_dict, xml_root):
+	for key in parsed_data_dict:
+		node = ET.SubElement(xml_root, "Node", name=data_dict[key][name], eid=str(data_dict[key][eid]))
 
-for key in data_dict:
-	data_dict[key] = replaceitem(data_dict[key], topmost, data_dict[key][managerid])
+		subs = parsed_data_dict[key][subords]
+		for sub in subs:
+			recurseThrough(sub, data_dict, node)
+	tree = ET.ElementTree(xml_root)
+	return tree
 
-print("$$$$$$$$$$$$$$$")
-print(json.dumps(parsed_data_dict, indent=4))
-topmosts = finditem(data_dict, topmost)
-print(topmosts)
+def createAndFillDbAndTable():
+	data_dict = {}
+	conn = sqlite3.connect('generic_employee.db')
+	conn.executescript('DROP TABLE IF EXISTS EMPLOYEES;')
+	conn.execute('''CREATE TABLE EMPLOYEES
+	         (ID INT PRIMARY KEY     NOT NULL,
+	         NAME           TEXT    NOT NULL,
+	         MANAGERID            INT);''')
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (1, 'A', 2 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (2, 'B', 4 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (3, 'C', 4 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (4, 'D', NULL )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (5, 'D', 6 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (6, 'E', 9 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (7, 'F', 8 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (8, 'G', 9 )");
+	conn.execute("INSERT INTO EMPLOYEES (ID,NAME,MANAGERID) VALUES (9, 'H', NULL )");
+	cursor = conn.execute("SELECT ID, NAME, MANAGERID from EMPLOYEES")
+	for row in cursor:
+		data_dict[row[0]] = {eid:row[0], name:row[1], managerid:row[2], subords:[]}
+
+	return data_dict
+
+
+if __name__ == "__main__":
+	data_dict = createAndFillDbAndTable()
+	try:
+		parsed_data_dict = parse_dict(data_dict)
+	except KeyError as e:
+		print("Manager heirarchy is invalid!\nCannot proceed with current setup.")
+		sys.exit(1)
+
+	# print(json.dumps(parsed_data_dict, indent=4))
+	# print("## ## ## ## ## ## ## ## ##")
+	# print(json.dumps(data_dict, indent=4))
+
+	root = ET.Element("EmpInfo")
+	tree = createXml(parsed_data_dict, data_dict, root)
+
+	tree.write(xml_file)
+
+	x = etree.parse(xml_file)
+	print(etree.tostring(x, pretty_print=True))
